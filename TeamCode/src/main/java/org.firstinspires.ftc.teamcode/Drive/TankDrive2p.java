218cc19e1db52308566c1d323c1638a464fe9a50
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-09 14:42:19 -0600

    Convert TankDrive2p.java to only be one file

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/TankDrive2p.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/TankDrive2p.java
@@ -2,54 +2,70 @@ package org.firstinspires.ftc.teamcode.Drive;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.Core.*;
 
 
 /**
- * ServoTest
- * Designed to find the best servo locations for opening and closing the claw.
+ * Lesson 1
+ * Teaches the basics of using the FTC package.
  */
 @TeleOp(name = "TankDrive2p", group = "auto")
 public class TankDrive2p extends OpMode {
-    /*/ Class variables
-    protected ClawCore clawCore;
-    protected DualMotorDrive dualMotorDrive;
+    // Class variables
+    protected DcMotor motorLeft;
+    protected DcMotor motorRight;
+    protected Servo clawLeft;
+    protected Servo clawRight;
 
     // Gamepad variables
-    protected boolean pgamepad2a;*/
+    protected boolean pgamepad2a;
 
     @Override
     public void init() {
-        /*clawCore = new ClawCore(hardwareMap);
-        dualMotorDrive = new DualMotorDrive(hardwareMap);
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        clawLeft = hardwareMap.get(Servo.class, "clawLeft");
+        clawRight = hardwareMap.get(Servo.class, "clawRight");
         // Telemetry
         telemetry.addData("STATUS: ", "Initialized"); // the FTC equivalent to println()
-        telemetry.addData("FTC Team #", "22531");*/
+        telemetry.addData("FTC Team #", "22531");
     }
 
     @Override
     public void loop() {
-        /*/ Update drive system
-        dualMotorDrive.move(gamepad1.left_stick_y, gamepad1.right_stick_y);
+        // Update drive system
+        motorLeft.setPower(gamepad1.left_stick_y);
+        motorRight.setPower(gamepad1.right_stick_y);
 
         // Update claw
         if (gamepad2.a) {
-            clawCore.open();
+            clawLeft.setPosition(0.595);
+            clawRight.setPosition(0.73);
         }
         if (gamepad2.b) {
-            clawCore.close();
+            clawLeft.setPosition(0.70);
+            clawRight.setPosition(0.61);
         }
-        //clawCore.update();
 
         // Telemetry
         telemetry(telemetry);
-
-        pgamepad2a = gamepad2.a;*/
     }
 
     public void telemetry(Telemetry telemetry) {
-        /*clawCore.telemetry(telemetry);
-        dualMotorDrive.telemetry(telemetry);*/
+        telemetry.addData("\nCurrent class", "DualMotorDrive.java");
+        telemetry.addData("runMode", motorLeft.getMode());
+        telemetry.addData("Left Power",
+                "%4.2f", motorLeft.getPower());
+        telemetry.addData("Right Power",
+                "%4.2f", motorRight.getPower());
+
+        telemetry.addData("\nCurrent class", "ClawCore.java");
+        telemetry.addData("Left claw position", clawLeft.getPosition());
+        telemetry.addData("Right claw position", clawRight.getPosition());
     }
 }
